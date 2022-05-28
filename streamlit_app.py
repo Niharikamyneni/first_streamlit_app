9b2abadcb14c66dcb17cd4da1cdfff5420514110
Niharikamyneni <95864270+Niharikamyneni@users.noreply.github.com>
2022-05-28 13:10:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,4 @@ streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg') 
 streamlit.text('🥑🍞 Avocado Toast') 
 
+streamlit.header('🍌🍓 Build your own Fruit Smoothie 🥝🍇')
